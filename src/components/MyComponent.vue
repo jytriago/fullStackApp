82c52f73ab27743cbd33c2f92e911e32192fea6b
Julio <ytriago26@gmail.com>
2025-01-07 15:38:19 -0400

    Cambiar la forma en que se modifica el atributo enabled del input en el componente MyComponent.vue

--- src/components/MyComponent.vue
+++ src/components/MyComponent.vue
@@ -14,15 +14,12 @@
                 </div>
                 <div class="flex-grow text-ellipsis whitespace-nowrap overflow-hidden">
                     <div class="text-xl font-medium text-black truncate"> {{ device["hostname"] }}</div>
-                    <p class="text-gray-500">{{ device.timeLeft + " seg" }}</p>
+                    <p class="text-gray-500">{{ device.timeLeft + " seg - " + switch_state[device.ss] }}</p>
                 </div>
 
                 <div class="flex-shrink-0">
                     <label class="inline-flex items-center cursor-pointer">
-
-                        <input v-if="device.ss == 0" type="checkbox" value="" class="sr-only peer" checked>
-                        <input v-else type="checkbox" value="" class="sr-only peer" >
-                        
+                        <input type="checkbox" value="" class="sr-only peer" v-model="device.ss">                        
                         <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
                         <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300"></span>
                     </label>
